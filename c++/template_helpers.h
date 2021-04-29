9911c6192c57807e61539eb123f1ef223b9b3d29
Lauri Jakku A <lja@lja.fi>
2021-04-29 15:52:54 +0300

    Vec on ok

--- c++/template_helpers.h
+++ c++/template_helpers.h
@@ -1,33 +1,15 @@
 #ifndef TEMPLATE_HELPERS_H
 #define TEMPLATE_HELPERS_H 1
 
-/* 
-	AUTHOR: Lauri Jakku <lja@lja.fi>
+#include <vector>
+#include <list>
 
-	Usage example:
-
-	withinRange<double> is_valid_percent {0, 100}; 
-	..
-	..
-	..
-	WithinRange<double> is_valid_percent {-100 , 100}; 
-	
-	bool a = is_valid_percent(50);
-	bool b = is_valid_percent(0.35);
-	bool c = is_valid_percent(101);
-	
-	cout << " a: " << a ;
-	cout << " b: " << b ;
-	cout << " c: " << c ;
-	cout << endl;
-
-*/
 
 template<typename T>
 class LessThan {
 
 		const T val; /* value compare against */
-
+		
 		public:
 			LessThan(const T& v) : val(v) {}
 			bool operator()(const T& x) const { return x < val; } /* Call operator */
@@ -38,7 +20,7 @@ template<typename T>
 class MoreThan {
 
 		const T val; /* value compare against */
-
+		
 		public:
 			MoreThan(const T& v) : val(v) {}
 			bool operator()(const T& x) const { return x > val; } /* Call operator */
@@ -50,11 +32,45 @@ class WithinRange {
 
 		const T valMin; /* value compare against */
 		const T valMax; /* value compare against */
-
+		
 		public:
 			WithinRange(const T& vmin, const T& vmax) : valMin(vmin),valMax(vmax) {}
 			bool operator()(const T& x) const { return (x >= valMin) && (x <= valMax); } /* Call operator */
 
 };
+
+using namespace std;
+
+/* Vector with range check build in */
+template<typename T>
+class Vec : public vector<T> {
+		
+	public:
+		using vector<T>::vector;
+
+		T& operator[](int i)
+			{ return vector<T>::at(i); }
+			
+		const T& operator[](int i) const
+			{ return vector<T>::at(i); }
+};
+
+/* Iterate and find all */
+template<typename T>
+using Iterator = typename T::iterator;
+
+
+template<typename C, typename V>
+vector<Iterator<C>> find_all(C& c, V v)
+{
+	vector<Iterator<C>> res;
+	for( auto p =c.begin(); p!= c.end(); ++p)
+	{
+		if (*p == v)
+			res.push_back(p);
+	}
+	return res;
+}
+
 
 #endif /* TEMPLATE_HELPERS_H */
